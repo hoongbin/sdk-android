1bf1e2c6895720a27c307feef24eb4cde493a541
lixiangyang <lixiangyang@himoca.com>
2020-07-31 16:33:44 +0800

    1.更新 把贝塔数据本地插件上传至jcenter.

--- demo/src/main/java/com/example/betadata_sdk_android/MainActivity.java
+++ demo/src/main/java/com/example/betadata_sdk_android/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.betadata_sdk_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.tv_hello_world).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "贝塔", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
